e50ee359d16e9f773fbe354a1fc139febe15f6e8
Daniel Studziński <stuniel@gmail.com>
2018-11-18 02:40:10 +0100

    Style slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -28,38 +28,23 @@ function SamplePrevArrow(props) {
 }
 
 class SliderGallery extends Component {
-  render() {
-    const { posts } = this.props;
+render() {
+    const { children, posts } = this.props;
     const settings = {
       className: "center",
       centerMode: true,
       infinite: true,
-      centerPadding: "20%",
-      slidesToShow: 1,
       swipeToSlide: true,
+      slidesToShow: 1,
       speed: 500,
+      variableWidth: true,
       nextArrow: <SampleNextArrow />,
       prevArrow: <SamplePrevArrow />
     };
     return (
       <div>
-        <Slider {...settings}>
-        {posts
-          .map(({ node: post }, index) => (
-            <div
-              className="content"
-              style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-              key={post.id}
-            >
-              <img src="https://scontent.fwaw3-1.fna.fbcdn.net/v/t1.0-9/1384237_676292945716987_1238664053_n.jpg?_nc_cat=106&_nc_ht=scontent.fwaw3-1.fna&oh=a745c57da05c03c59802942e7d024370&oe=5C7B6A25" />
-              <p>
-                <strong>0{index + 1} </strong>
-                <Link className="has-text-primary" to={post.fields.slug}>
-                  {post.frontmatter.title}
-                </Link>
-              </p>
-            </div>
-          ))}
+        <Slider {...settings} ref={node => { this.slider = node }}>
+        {children}
         </Slider>
       </div>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,11 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Slider from '../components/Slider'
 
+function formatIndex(index) {
+  const number = index + 1;
+  return number > 9 ? number : `0${number}`;
+}
+
 export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
@@ -14,7 +19,24 @@ export default class IndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <Slider posts={posts}/>
+              <Slider posts={posts}>
+                {posts
+                  .map(({ node: post }, index) => (
+                    <div
+                      className="content"
+                      style={{ border: '1px solid #eaecee', padding: '2em 4em', height: '100%' }}
+                      key={post.id}
+                    >
+                      <img className="cover-image" src={post.frontmatter.cover} />
+                      <p>
+                        <strong>{formatIndex(index)} </strong>
+                        <Link className="has-text-primary wrapword" to={post.fields.slug}>
+                          {post.frontmatter.title}
+                        </Link>
+                      </p>
+                    </div>
+                  ))}
+              </Slider>
             </div>
           </div>
         </section>
@@ -48,6 +70,7 @@ export const pageQuery = graphql`
             title
             templateKey
             date(formatString: "MMMM DD, YYYY")
+            cover
           }
         }
       }
